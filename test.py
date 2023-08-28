d2b887ef46b5ddb72493d666465f4a94083ea6d7
y.mo <97536008+yaxinm@users.noreply.github.com>
2023-08-28 15:55:08 +0100

    backup

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
-from MD_AE_tools.models.models_no_bias import Autoencoder_ff
-from pathlib import Path 
+import matplotlib.pyplot as plt
 
-w = Path('../../Results/MD-CNN-AE/experiment_ff_ae_data/no_cutoff_10-3792699/weights.h5')
-# print(w.exists())
+plt.style.use('a4.mplstyle')
 
-mdl=Autoencoder_ff(1008,2,[450,150,50],act_fct='relu',drop_rate=0.12)
-
-mdl.build((None,1008))
-mdl.summary()
-mdl.load_weights(w)
-
+plt.figure()
+plt.plot([0,1],[0,1])
+plt.xlabel('$a$')
+plt.show()
