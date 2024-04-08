74e85d7662dafaa427084d73a4593bf91f1d5981
y.mo <97536008+yaxinm@users.noreply.github.com>
2024-04-08 12:12:44 +0100

    new cmap figures

--- project_specific_utils/__init__.py
+++ project_specific_utils/__init__.py
@@ -1,3 +1,6 @@
 '''
 A collection of project specific functions. 
 '''
+from .plot import create_custom_colormap
+my_discrete_cmap = create_custom_colormap(map_name='overleaf-earth',type='discrete')
+my_continuous_cmap = create_custom_colormap(map_name='overleaf-earth',type='continuous')
